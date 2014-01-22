08d640d44871fdbdfee00d9932cb8ed67c85025d
Nicholas Terry <nick.i.terry@gmail.com>
2014-01-21 20:13:29 -0700

    Added basic example code.

---
+++ NancyDemo/FooModule.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Nancy;
+
+
+namespace NancyDemo
+{
+    public class FooModule : NancyModule
+    {
+        public FooModule()
+        {
+            //Super awesome schizophrenic bunny
+            Get["/"] = _ =>
+            {
+                return new Response
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    ContentType = "text/plain",
+                    Contents = stream =>
+                    {
+                        using (var writer = new StreamWriter(stream))
+                        {
+                            writer.Write("   (\\_/)\n   (0_0)\n  C(\")(\")\nMoo! I'm a cow!");
+                        };
+                    }
+                };
+            };
+
+            //Just plain OK response
+            Get["/awesome"] = _ =>
+            {
+                return new Response
+                {
+                    StatusCode = HttpStatusCode.OK
+                };
+            };
+        }
+    }
+}
---
+++ NancyDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+
+namespace NancyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new NancyHost(new Uri("http://localhost:8080"));
+            host.Start();
+            Console.ReadLine();
+        }
+    }
+}
